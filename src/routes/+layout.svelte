b7e76fb7f53567f49d7a07c44c4d60d0aeebab07
Jordan Ayotte <jordanayotte13@outlook.com>
2025-01-30 16:16:23 -0500

    theme changer base

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	
 </script>
-
+<div class={`bg-primary text-tBase theme-orange`}>
 {@render children()}
+</div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,10 +2,15 @@ import type { Config } from 'tailwindcss';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
+	plugins: [],
 	theme: {
-		extend: {}
-	},
-
-	plugins: []
+		extend: {
+			colors: {
+				primary: "var(--color-primary)",
+				secondary: "var(--color-primary)",
+				bgPrimary: "var(--color-bg-primary)",
+				tBase: "var(--color-text-base)"
+			}
+		}
+	}
 } satisfies Config;
