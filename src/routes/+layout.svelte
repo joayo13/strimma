45673a34b2daec816e8e93b9de36abd3eadd9565
Jordan Ayotte <jordanayotte13@outlook.com>
2025-02-02 23:02:47 -0500

    basic navbar layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	let { children } = $props();
 	
 </script>
+<nav class="px-8 py-2 flex justify-between items-center">
+	<a href="/" class="text-3xl md:text-6xl">streakr</a>
+	<div class="w-12 h-12 md:w-20 md:h-20  rounded-full bg-neutral-500"></div>
+</nav>
 <div class={`bg-primary text-tBase theme-orange`}>
 {@render children()}
 </div>
