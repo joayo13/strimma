f23ae273593843e615d4c40dcb1188ff90f68e0f
Jordan Ayotte <jordanayotte13@outlook.com>
2025-02-06 15:58:01 -0500

    supabase integration from tutorial complete

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,21 @@
 <script lang="ts">
 	import '../app.css';
 	import SettingsModal from '../components/SettingsModal.svelte';
-	let { children } = $props();
+	import { invalidate } from '$app/navigation'
+  	import { onMount } from 'svelte'
 	let theme = $state('dark')
+  	let { data, children } = $props()
+  	let { session, supabase } = $derived(data)
+
+  onMount(() => {
+    const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+      if (newSession?.expires_at !== session?.expires_at) {
+        invalidate('supabase:auth')
+      }
+    })
+
+    return () => data.subscription.unsubscribe()
+  })
 </script>
 <nav class={`px-8 py-2 fixed flex w-full justify-between items-center bg-primary text-tBase theme-${theme}`}>
 	<a href="/" class="text-3xl font-bold md:text-6xl">streakr</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,14 @@
+
 <script>
-	import NewStreakModal from "../components/NewStreakModal.svelte";
-</script>
-<NewStreakModal/>
+	import NewStreakModal from '../components/NewStreakModal.svelte';
+	let { data } = $props()
+	let { colors } = $derived(data)
+  </script>
+  
+  <h1>Welcome to Supabase!</h1>
+  <ul>
+	{#each colors as color}
+	  <li>{color.name}</li>
+	{/each}
+  </ul>
+  <NewStreakModal/>
