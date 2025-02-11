3ebdb032f10c47bfd35e0e39fd8e79e729254bc3
Jordan Ayotte <jordanayotte13@outlook.com>
2025-02-11 12:39:03 -0500

    moved streak code into streak component

--- src/components/NewStreakModal.svelte
+++ src/components/NewStreakModal.svelte
@@ -1,4 +1,7 @@
    <script lang="ts">
+	import { invalidate } from "$app/navigation";
+	import type { EventHandler } from "svelte/elements";
+  let {supabase, streaks} = $props()
   
     let settingsDialog: HTMLDialogElement;
   
@@ -9,6 +12,21 @@
     function close() {
       settingsDialog.close();
     }
+    const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = async (evt) => {
+      evt.preventDefault()
+      if (!evt.target) return
+  
+      const form = evt.target as HTMLFormElement
+  
+      const streak = (new FormData(form).get('streak_name') ?? '') as string
+      if (!streak) return
+  
+      const { error } = await supabase.from('streaks').insert({ streak_name: streak, streak_days: 0})
+      if (error) console.error(error)
+  
+      invalidate('supabase:db:streaks')
+      form.reset()
+    }
   </script>
   
   <button onclick={open} class="hover:bg-secondary transition-colors px-4 py-2 rounded-lg">+ Add New Streak</button>
@@ -16,7 +34,12 @@
   <dialog bind:this={settingsDialog} class="backdrop:bg-black/50 rounded-lg overflow-hidden text-tBase">
     <div class={`bg-primary p-6`}>
       <h2 class="text-xl font-bold mb-4">Add New Streak</h2>
-      <p>options...</p>
+      <form onsubmit={handleSubmit}>
+        <label>
+          Add a streak
+          <input name="streak_name" type="text" />
+        </label>
+      </form>
       <button onclick={close} class="mt-4 px-4 py-2 bg-secondary rounded-md">
         Close
       </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 
 <script lang="ts">
-	import { invalidate } from '$app/navigation';
-	import type { EventHandler } from 'svelte/elements';
 	import NewStreakModal from '../components/NewStreakModal.svelte';
 	let { data } = $props()
 	let { streaks, supabase, user } = $derived(data)
@@ -13,40 +11,18 @@
       }
 	  window.location.reload()
     }
-	const handleSubmit: EventHandler<SubmitEvent, HTMLFormElement> = async (evt) => {
-      evt.preventDefault()
-      if (!evt.target) return
-  
-      const form = evt.target as HTMLFormElement
-  
-      const streak = (new FormData(form).get('streak_name') ?? '') as string
-      if (!streak) return
-  
-      const { error } = await supabase.from('streaks').insert({ streak_name: streak, streak_days: 0})
-      if (error) console.error(error)
-  
-      invalidate('supabase:db:streaks')
-      form.reset()
-    }
+	
   </script>
-  
-  <h1>Private page for user: {user?.email}</h1>
-  <h2>Notes</h2>
   <ul>
     {#each streaks as streak}
       <li>{streak.streak_name + streak.streak_days}</li>
 
     {/each}
   </ul>
-  <form onsubmit={handleSubmit}>
-    <label>
-      Add a streak
-      <input name="streak_name" type="text" />
-    </label>
-  </form>
+  
   <button onclick={logout}>Logout</button>
     
   
     
   
-  <NewStreakModal/>
+  <NewStreakModal supabase={supabase} streaks={streaks}/>
