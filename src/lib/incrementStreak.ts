36cd7842931a1811792a6fcbc3803df6fb92e4f7
Jordan Ayotte <jordanayotte13@outlook.com>
2025-02-16 12:47:21 -0500

    streak days can be accumulated from zero

--- src/lib/incrementStreak.ts
+++ src/lib/incrementStreak.ts
@@ -5,7 +5,7 @@ export function incrementStreak(streak: { id: number; streak_name: string; strea
     // Compare current date and last updated date in terms of calendar day (UTC)
     const isNextDay = now.getUTCDate() > lastUpdated.getUTCDate()
     // If it is the next day, increment the streak and update last_updated
-    return isNextDay
+    return isNextDay || streak.streak_days === 0
       ? { ...streak, streak_days: streak.streak_days + 1, last_updated: now.toISOString() }
       : streak;
   }
