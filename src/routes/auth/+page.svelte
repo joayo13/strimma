bdf766ccec523fbe9c2b69d20e6c08326067c67b
Jordan Ayotte <jordanayotte13@outlook.com>
2025-02-25 10:53:27 -0500

    login form updated

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,12 +1,20 @@
-<form method="POST" action="?/login">
-    <label>
+<script>
+	import PrimaryButton from "../../components/buttons/PrimaryButton.svelte";
+	import SecondaryButton from "../../components/buttons/SecondaryButton.svelte";
+
+</script>
+<form class="flex flex-col gap-2" method="POST" action="?/login">
+    <label for="email" class="block">
       Email
-      <input name="email" type="email" />
     </label>
-    <label>
+      <input required name="email" type="email" class="bg-primary text-tPrimary indent-2 text-xl px-2 py-2 rounded-lg"/>
+    
+    <label for="password">
       Password
-      <input name="password" type="password" />
     </label>
-    <button>Login</button>
-    <button formaction="?/signup">Sign up</button>
+      <input required name="password" type="password" class="bg-primary text-tPrimary indent-2 text-xl px-2 py-2 rounded-lg"/>
+    <div class="flex justify-between">
+    <PrimaryButton title="Login"></PrimaryButton>
+    <SecondaryButton title="Sign Up" formaction="?/signup"></SecondaryButton>
+  </div>
   </form>
