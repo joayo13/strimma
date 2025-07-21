f52eaeb5c90371418b03690d69dfbe8f06a69bd1
Jordan Ayotte <jordanayotte13@outlook.com>
2025-07-21 11:40:07 -0400

    changed btn text to conitnue

--- src/components/buttons/SignInWithGoogleButton.svelte
+++ src/components/buttons/SignInWithGoogleButton.svelte
@@ -3,5 +3,5 @@
 	class="mt-4 flex w-full items-center justify-center gap-2 rounded-md bg-primary px-4 py-2 transition-all hover:brightness-110"
 >
 	<img src="/google.svg" alt="Google logo" class="h-5 w-5" />
-	Sign In With Google
+	Continue With Google
 </button>
